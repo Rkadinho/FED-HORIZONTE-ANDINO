f9aea196dd9a692818fc5de2f2eb58e95edab474
rkadinho <ricardolopesroj@gmail.com>
2025-01-24 13:55:49 -0300

    criação da pagina home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/headers";
+import home from "../assets/home.jpeg"
+import '../css/pages/home.css';
 
 function Home() {
   return (
     <>
       <Header></Header>
       <div>
-        
+        <img src={home} className="homeImg"></img>
       </div>
     </>
   )
